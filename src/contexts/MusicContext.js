dc2f29c7dcc0b3e799af805cd156dc746da4e88c
Joanna Huang <joannathhuang@gmail.com>
2020-11-09 18:10:05 -0500

    [Feat]: starting on music side

---
+++ src/contexts/MusicContext.js
@@ -0,0 +1,13 @@
+// import React, { createContext, useState, useEffect } from 'react';
+
+// export const musicContext = createContext()
+
+// const API_KEY = process.env.SPOTIFY_API_KEY
+// const API_URL = `https://api.spotify.com/`
+
+// const MusicContextProvider = () => {
+//     const [url, setUrl] = useState(API_URL)
+//     return (  );
+// }
+
+// export default MusicContextProvider;
